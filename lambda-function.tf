50ae4a07a78090dbec0e68730ac993f2deb7bbb6
Ade-Alawiye <69054624+Ade-Alawiye@users.noreply.github.com>
2021-06-21 07:12:09 -0600

    Add files via upload

---
+++ lambda-function.tf
@@ -0,0 +1,28 @@
+locals {
+  lambda_zip_location = "outputs/lambda-greet.zip"
+}
+
+data "archive_file" "lambda-greet" {
+  type        = "zip"
+  source_file = "greet_lambda.py"
+  output_path = "${local.lambda_zip_location}"
+}
+
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "${local.lambda_zip_location}"
+  function_name = "lambda-greet"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "greet_lambda.lambda_handler"
+
+
+  source_code_hash = "${filebase64sha256(local.lambda_zip_location)}"
+
+  runtime = "python3.6"
+
+  environment {
+    variables = {
+      greeting = "UDACITY ROCKS"
+    }
+  }
+}
